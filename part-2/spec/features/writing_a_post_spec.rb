3d5bb388e3f34bb707bbef8acf799fe8ff8daf51
Sam Blackman <sblackman@gmail.com>
2015-06-24 02:29:20 -0400

    WIP write feature tests for sync and async like buttons

--- part-2/spec/features/writing_a_post_spec.rb
+++ part-2/spec/features/writing_a_post_spec.rb
@@ -6,14 +6,12 @@ feature "writing a new post" do
   end
 
   context "without javascript" do
-    scenario "user navigates to post form, submits it, and sees the post", { js: false } do
+    scenario "user fills in the post form, submits it, and sees the post", { js: false } do
       visit "/"
       expect(page).to have_content "Share Anything"
       expect(page).to have_content @previous_post.title
-      click_link("Share Your Thoughts")
 
-
-      expect(page).to have_content "Write Your Post"
+      expect(page).to have_content "Share Your Thoughts"
       within("#post_form") do
         fill_in "post[title]", with: "Bright Idea"
         fill_in "post[author_name]", with: "Evelyn"
@@ -25,26 +23,29 @@ feature "writing a new post" do
       expect(page).to have_content "Bright Idea"
       expect(page).to have_content "Evelyn"
       expect(page).to have_content "I've been doing a lot of thinking ..."
+      expect(page).to have_content "0 likes"
+    end
+
+    scenario "user clicks on the like button and the likes on that post increase" do
+      visit "/"
+      expect(page).to have_content "Share Anything"
+      expect(page).to have_content "Share Your Thoughts"
+      expect(page).to have_content @previous_post.title
+
+      find(".post:first-child .post_like_button").click
+      expect(page).to_not have_content("Share Your Thoughts")
+      expect(page).to have_content "The Post ..."
+      expect(page).to have_content @previous_post.title
+      expect(page).to have_content @previous_post.author_name
+      expect(page).to have_content "1 likes"
     end
   end
 
   context "with javascript" do
-    scenario "user navigates to post form, submits it, and sees the post", { js: true } do
+    scenario "user fills in the post form, submits it, and sees the post", { js: true } do
       visit "/"
       expect(page).to have_content "Share Anything"
       expect(page).to have_content @previous_post.title
-
-
-      click_link("Share Your Thoughts")
-      # Stay on page
-      expect(page).to have_content(@previous_post.title), "Clicking the 'Share Your Thoughts' link should not navigate to a new page."
-      # Link goes away
-      expect(page).to_not have_content("Share Your Thoughts"), "The 'Share Your Thoughts' link should be hidden."
-      # Add the post form to the page
-      expect(page).to have_selector("#post_form"), "The post form should be on the page."
-      # Don't render the posts/new template
-      expect(page).to_not have_content("Write Your Post"), "Just the form for creating a new post should be added to the page."
-
 
       # Fill in the form
       within("#post_form") do
@@ -56,7 +57,6 @@ feature "writing a new post" do
       # Submit the form
       find("input[name='submit']").click
 
-
       # Submitting the form stays on the page
       expect(page).to have_content(@previous_post.title), "Submitting the form should not navigate to a new page."
       # New post is added to the DOM
@@ -65,11 +65,18 @@ feature "writing a new post" do
       expect(page).to have_content("I've been doing a lot of thinking ..."), "The new post should be added to the page."
       # Don't render the posts/show template
       expect(page).to_not have_content("The Post ..."), "Just the new post should be added to the page."
+    end
 
+    scenario "user clicks on the like button and the likes on that post increase" do
+      visit "/"
+      expect(page).to have_content "Share Anything"
+      expect(page).to have_content "Share Your Thoughts"
+      expect(page).to have_content @previous_post.title
 
-      # Remove form and show link again
-      expect(page).to_not have_selector("#post_form"), "The post form should be removed from the DOM."
-      expect(page).to have_content("Share Your Thoughts"), "The 'Share Your Thoughts' link should be visible."
+      find(".post:first-child .post_like_button").click
+      expect(page).to_not have_content("The Post ..."), "Clicking the 'like' button should not navigate to a new page."
+      expect(page).to have_content @previous_post.title
+      expect(page).to have_content "1 likes"
     end
   end
 end
