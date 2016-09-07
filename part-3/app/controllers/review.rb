91eeac2da89b0a7eb8441f9399886212fcbd3923
Ephraim Friedman <ephraimfried@gmail.com>
2016-09-07 17:42:54 -0400

    refactor more and in middle of adding new feature

--- part-3/app/controllers/review.rb
+++ part-3/app/controllers/review.rb
@@ -2,11 +2,11 @@ post '/restaurants/:id/reviews' do
 	logged_out?
 	@restaurant = Restaurant.find(params[:id])
 	
-	redirect '/' if @restaurant.aready_reviewed(current_user)
+	redirect '/' if @restaurant.already_reviewed(current_user)
 
 	review = @restaurant.reviews.build(rating: params[:rating].to_i, body:params[:body] )
 	review.author = current_user
-	
+
 	if review.save
 		@message = "<p>Thak you for leaving a review.</p>"
 		erb :'restaurants/show'
--- part-3/app/models/review.rb
+++ part-3/app/models/review.rb
@@ -2,7 +2,9 @@ class Review < ActiveRecord::Base
   belongs_to :restaurant
   belongs_to :author, class_name: "User", foreign_key: :user_id
 
-  validates :rating, :body, :restaurant, :author, presence: true
+  validates :body, :restaurant, :author, presence: true
+
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 1 }
 
   validates :author, uniqueness: { scope: :restaurant}
 
