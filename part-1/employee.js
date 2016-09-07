aad37e9bd1bbdba60e8f5814f1358a27092ca604
Ephraim Friedman <ephraimfried@gmail.com>
2016-09-07 09:14:25 -0400

    complete release 0

--- part-1/employee.js
+++ part-1/employee.js
@@ -1,1 +1,16 @@
+var Employee = function(args) {
+	this.firstName = args["firstName"] || ""
+	this.sales = args["sales"] || []
+}
 
+Employee.prototype.totalSales = function() {
+	var sum = 0
+	for(i = 0; i < this.sales.length; i++){
+		sum += this.sales[i]
+	}
+	return sum
+}
+
+Employee.prototype.averageSales = function() {
+	return this.totalSales() / this.sales.length
+}
